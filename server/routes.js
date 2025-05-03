4bf1f859e0299b1b2bce1185e274cf6862a13bf3
bekicom <adelkauz6@gmail.com>
2025-05-03 14:50:14 +0500

    d

--- server/routes.js
+++ server/routes.js
@@ -1,29 +1,68 @@
 const express = require("express");
 const rt = express.Router();
-const { createUser, loginUser, getUsers } = require("./controllers/userController");
-const { createProductType, getProductTypes, updateProductType, deleteProductType } = require("./controllers/productTypeController");
-const { createProduct, getProducts, updateProduct, deleteProduct } = require("./controllers/productController");
-const { createSale, getSales, createPayment, deliverSale } = require("./controllers/saleController");
+
+// ✅ Controllerlar
+const {
+  createUser,
+  loginUser,
+  getUsers,
+} = require("./controllers/userController");
+
+const {
+  createProductType,
+  getProductTypes,
+  updateProductType,
+  deleteProductType,
+} = require("./controllers/productTypeController");
+
+const {
+  createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+} = require("./controllers/productController");
+
+const {
+  createSale,
+  getSales,
+  createPayment,
+  deliverSale,
+  approveSale, // ✅ Yangi tasdiqlash controller
+} = require("./controllers/saleController");
+
+// ✅ Middleware
 const { authMiddleware } = require("./middlewares/authMiddleware");
 
-rt.post('/user/create', authMiddleware, createUser)
-rt.post('/user/login', loginUser)
-rt.get('/user/get',authMiddleware, getUsers)
+// ✅ User
+rt.post("/user/create", authMiddleware, createUser);
+rt.post("/user/login", loginUser);
+rt.get("/user/get", authMiddleware, getUsers);
 
-rt.post('/product-type/create', authMiddleware, createProductType)
-rt.get('/product-type/get', authMiddleware, getProductTypes)
-rt.put('/product-type/update/:productTypeId', authMiddleware, updateProductType)
-rt.delete('/product-type/delete/:productTypeId', authMiddleware, deleteProductType)
+// ✅ Product type
+rt.post("/product-type/create", authMiddleware, createProductType);
+rt.get("/product-type/get", authMiddleware, getProductTypes);
+rt.put(
+  "/product-type/update/:productTypeId",
+  authMiddleware,
+  updateProductType
+);
+rt.delete(
+  "/product-type/delete/:productTypeId",
+  authMiddleware,
+  deleteProductType
+);
 
-rt.post('/product/create', authMiddleware, createProduct)
-rt.get('/product/get', authMiddleware, getProducts)
-rt.put('/product/update/:productId', authMiddleware, updateProduct)
-rt.delete('/product/delete/:productId', authMiddleware, deleteProduct)
+// ✅ Product
+rt.post("/product/create", authMiddleware, createProduct);
+rt.get("/product/get", authMiddleware, getProducts);
+rt.put("/product/update/:productId", authMiddleware, updateProduct);
+rt.delete("/product/delete/:productId", authMiddleware, deleteProduct);
 
-rt.post('/sale/create', authMiddleware, createSale)
-rt.get('/sale/get', authMiddleware, getSales)
-rt.put('/sale/payment/:saleId', authMiddleware, createPayment)
-rt.put('/sale/deliver/:saleId', authMiddleware, deliverSale)
-
+// ✅ Sale
+rt.post("/sale/create", authMiddleware, createSale);
+rt.get("/sale/get", authMiddleware, getSales);
+rt.put("/sale/payment/:saleId", authMiddleware, createPayment);
+rt.put("/sale/deliver/:saleId", authMiddleware, deliverSale);
+rt.put("/sale/approve/:saleId", authMiddleware, approveSale); // ✅ Yangi qo‘shildi
 
 module.exports = rt;
