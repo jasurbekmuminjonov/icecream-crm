a99fa4ff0b5d5bfefad94c8069f9106752e6206b
bekicom <adelkauz6@gmail.com>
2025-05-07 16:09:07 +0500

    ddd

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,28 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     fullname: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        match: /^\+998\d{9}$/,
-        unique: true
+      type: String,
+      match: /^\+998\d{9}$/,
+      unique: true,
     },
     password: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     role: {
-        type: String,
-        enum: ["admin", "manager", "distributor", 'client']
+      type: String,
+      enum: ["admin", "manager", "distributor", "client"],
     },
     adminId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        default: null
-    }
-},{timestamps:true});
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    location: {
+      lat: { type: Number, default: null },
+      lng: { type: Number, default: null },
+      addedAt: { type: Date, default: null },
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
